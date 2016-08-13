2e92463d20434080e22397ce7441c414d9023323
javier.jose.llinares@gmail.com <Javier2662>
2016-08-13 12:08:01 -0500

    Se modifica el archivo de construccion

--- src/test/java/org/jllinares/microservice/CalculadoraServiceTests.java
+++ src/test/java/org/jllinares/microservice/CalculadoraServiceTests.java
@@ -14,7 +14,6 @@ public class CalculadoraServiceTests {
   @Test
   public void test_sum() {
     CalculadoraService service = new CalculadoraService();
-
     assertEquals("La suma de 1 y 3 debe ser 3", Integer.valueOf(3), service.sum(1, 2));
   }
 }
