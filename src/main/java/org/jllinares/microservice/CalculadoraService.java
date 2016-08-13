3711efb28556e76b11c5f800db98bca6865ea836
javier.jose.llinares@gmail.com <Javier2662>
2016-08-13 11:59:12 -0500

    Envio de informacio

--- src/main/java/org/jllinares/microservice/CalculadoraService.java
+++ src/main/java/org/jllinares/microservice/CalculadoraService.java
@@ -1,5 +1,32 @@
 package org.jllinares.microservice;
 
-public class CalculadoraService {
+import org.springframework.stereotype.Service;
 
+/**
+ * The Class CalculadoraService.
+ */
+@Service
+public class CalculadoraService {
+  
+  /**
+   * Sum.
+   *
+   * @param ope1 the ope 1
+   * @param ope2 the ope 2
+   * @return the integer
+   */
+  public Integer sum(Integer ope1, Integer ope2) {
+    return ope1 + ope2;
+  }
+
+  /**
+   * Subs.
+   *
+   * @param ope1 the ope 1
+   * @param ope2 the ope 2
+   * @return the integer
+   */
+  public Integer subs(Integer ope1, Integer ope2) {
+    return ope2 - ope1;
+  }
 }
---
+++ src/test/java/org/jllinares/microservice/CalculadoraServiceTests.java
@@ -0,0 +1,20 @@
+package org.jllinares.microservice;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * The Class CalculadoraServiceTests.
+ */
+public class CalculadoraServiceTests {
+  
+  /**
+   * Test sum.
+   */
+  @Test
+  public void test_sum() {
+    CalculadoraService service = new CalculadoraService();
+
+    assertEquals("La suma de 1 y 3 debe ser 3", Integer.valueOf(3), service.sum(1, 2));
+  }
+}
