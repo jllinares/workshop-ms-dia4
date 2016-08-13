f6a7425ae6cb11004d036e967bc9224425bfaece
javier.jose.llinares@gmail.com <Javier2662>
2016-08-13 12:39:07 -0500

    Actualizacion del codigo de pruebas

--- src/test/java/org/jllinares/microservice/CalculadoraServiceTests.java
+++ src/test/java/org/jllinares/microservice/CalculadoraServiceTests.java
@@ -16,4 +16,14 @@ public class CalculadoraServiceTests {
     CalculadoraService service = new CalculadoraService();
     assertEquals("La suma de 1 y 3 debe ser 3", Integer.valueOf(3), service.sum(1, 2));
   }
+  
+  /**
+   * Test subs.
+   */
+  @Test
+  public void test_subs() {
+    CalculadoraService service = new CalculadoraService();
+
+    assertEquals("La resta de 2 con 1 debe ser 1", Integer.valueOf(1), service.subs(1, 2));
+  }
 }
